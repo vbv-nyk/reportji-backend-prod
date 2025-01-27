5735b17b2421284c3da98beb0693be7b410afa97
Vaibhav Nayak <vbv.nyk.2003@gmail.com>
2025-01-27 14:35:22 +0530

    Bug fixes

--- llm/reportGeneratorPrompt.ts
+++
@@ -1,3 +0,0 @@
-export const reportGeneratorPrompt = `
-You're supposed to generate the entire report in the provided format. Make sure every section has a lot of images and the figures are mentioned.
-`
